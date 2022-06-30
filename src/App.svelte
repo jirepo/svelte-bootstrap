f7e72aa6663243653137916cfab3ba60f70eb422
nowsogood <latteonterrace@gmail.com>
2022-06-30 16:41:50 +0900

    부트스트랩 정리

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	export let name: string;
+import NaonCard from './component/NaonCard.svelte';
+import NaonBaseButton from './component/NaonBaseButton.svelte';
 </script>
 
-<main class="container">
-    <h1 class="text-secondary">Hello {name}!</h1>
-
-    <div>
-        <p class="text-primary">SASS is working!</p>
+<div class="main">
+    <div >
+        top 
     </div>
-</main>
+    <div>
+        <NaonCard/>
+    </div>
+    <div>
+        <NaonBaseButton/>
+    </div>
+</div>
 
+<style>
+    .main { 
+        display: flex; 
+    }
 
-<button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
-<button type="button" class="btn btn-warning">Warning</button>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-light">Light</button>
-<button type="button" class="btn btn-dark">Dark</button>
-
-<button type="button" class="btn btn-link">Link</button>
+</style>
---
+++ src/component/NaonBaseButton.svelte
@@ -0,0 +1,17 @@
+<script>
+
+const onclick = () => { 
+    alert("click");
+}
+</script>
+
+<button type="button"  on:click={onclick} class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
